eb8a0260f475f1d7ea24ec0da165c507ffe0f684
edgartrs <edgartrs@users.noreply.github.com>
2023-07-30 01:26:48 -0500

    Add EF core (#2)
    * feat: Add EF core
    
    * Add initial Chirp Model (wip)
    
    * Add endpoints to create chirps and retrieve them

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Chirps.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<ChirpsDbContext>(options => options.UseInMemoryDatabase("Chirps"));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -8,9 +11,13 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "Chirps API v0.0.1");
 
-app.MapGet("/chirps", () => Results.Ok(new[] {
-    new { Id = 1, Message = "Hello World!" },
-    new { Id = 2, Message = "Goodbye cruel world..." }
-}));
-
+app.MapGet("/chirps", async (ChirpsDbContext db) => await db.Chirps.ToListAsync());
+app.MapGet("/chirps/{id}", async (Guid id, ChirpsDbContext db) => await db.Chirps.FindAsync(id) is Chirp chirp ? Results.Ok(chirp) : Results.NotFound());
+app.MapPost("/chirps", async (string message, ChirpsDbContext db) =>
+{
+  var chirp = new Chirp { Message = message, CreatedAt = DateTime.Now, Id = Guid.NewGuid() };
+  await db.Chirps.AddAsync(chirp);
+  await db.SaveChangesAsync();
+  return Results.Created($"/chirps/{chirp.Id}", chirp);
+});
 app.Run();
